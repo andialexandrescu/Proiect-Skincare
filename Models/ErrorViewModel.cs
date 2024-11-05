269e59f3377f702fd9b1a7ef0e95267683fd25a2
Teodora Nitu <nitu.teo@gmail.com>
2024-11-05 16:54:00 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proiect_skincare.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
